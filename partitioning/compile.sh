b0af27c635cd7e983bb1b8f07e6badbe0d0d54c2
emiljapelt <55390848+emiljapelt@users.noreply.github.com>
2023-03-01 16:02:15 +0100

    Implement count_then_move partitioning

--- partitioning/compile.sh
+++ partitioning/compile.sh
@@ -1,2 +1,2 @@
 gcc -pthread ./utils.c ./partitioning.c -o ./partitioning.exe
-# Add -Wall -g3 -fsanitize=address for address debugging
+# Add '-Wall -g3 -fsanitize=address' for address debugging
--- partitioning/partitioning.c
+++ partitioning/partitioning.c
@@ -4,67 +4,91 @@
 #include <sys/random.h>
 #include <sys/wait.h>
 #include <pthread.h>
+#include <stdatomic.h>
 #include <unistd.h>
 
 #include "utils.h"
 
-void partition_sequential(uint64* input, uint64** output, uint64 start_index, uint64 work_size, uint64 partition_count) {
+void count_then_move(uint64 thread_id, uint64 thread_count, uint64* count_reports, uint64* input, uint64* output, uint64 start_index, uint64 work_size, uint64 partition_count, _Atomic uint64* counted) {
 
-    uint64* partitions = malloc((sizeof(uint64) * partition_count) + ((2 * sizeof(uint64)) * work_size));
-
-    uint64 hashes[work_size];
-    for(int i = 0; i < partition_count; i++) partitions[i] = 0;
+// Count partition contributions
+    for(uint64 i = 0; i < partition_count; i++) count_reports[(thread_id*partition_count)+i] = 0;
     for(uint64 i = 0; i < work_size; i++) {
-        hashes[i] = hash(input[start_index+(2*i)]);
-        partitions[hashes[i]]++;
+        count_reports[(thread_id*partition_count) + hash(input[start_index+(2*i)])]++;
     }
 
-    int partition_indices[partition_count];
-    for(int i = 0; i < partition_count; i++) partition_indices[i] = 0;
+// Thread synchronization
+    (*counted)++;
+    while (*counted != -1);
 
-    for(uint64 i = 0; i < work_size; i++) {
-        uint64 hash = hashes[i];
-        uint64 section_offset = partition_count;
-        for(int h = 0; h < hash; h++) section_offset += 2 * partitions[h];
-        uint64 partition_index = 2 * partition_indices[hash]++;
-        partitions[section_offset + partition_index] = input[start_index+(2*i)];
-        partitions[section_offset + partition_index + 1] = input[start_index+(2*i)+1];
+// Calculate partition starting indecies
+    uint64 partition_starts[partition_count];
+    partition_starts[0] = 0;
+    for(uint64 p = 1; p < partition_count; p++) {
+        partition_starts[p] = partition_starts[p-1];
+        for(uint64 t = 0; t < thread_count; t++) 
+            partition_starts[p] += 2 * count_reports[(t*partition_count)+(p-1)];
     }
 
-    *output = partitions;
+// Calculate initial partition offsets
+    uint64 partition_offsets[partition_count];
+    for(uint64 o = 0; o < partition_count; o++) partition_offsets[o] = 0;
+    for(uint64 p = 0; p < partition_count; p++) {
+        for(uint64 t = 0; t < thread_id; t++) partition_offsets[p] += 2*count_reports[(t*partition_count)+p];
+    }
+
+// Move data to output
+    for(uint64 i = 0; i < work_size; i++) {
+        uint64 h = hash(input[start_index+(2*i)]);
+        output[partition_starts[h]+partition_offsets[h]] = input[start_index+(2*i)];
+        output[partition_starts[h]+partition_offsets[h]+1] = input[start_index+(2*i)+1];
+        partition_offsets[h] += 2;
+    }
 }
 
-uint64* create_args(uint64* input, uint64** output, uint64 start_index, uint64 work_size, uint64 partition_count) {
-    uint64* alloc = malloc(5 * 8);
-    alloc[0] = (uint64)input;
-    alloc[1] = (uint64)output;
-    alloc[2] = start_index;
-    alloc[3] = work_size;
-    alloc[4] = partition_count;
+uint64* create_args(uint64 thread_id, uint64 thread_count, uint64* count_reports, uint64* input, uint64* output, uint64 start_index, uint64 work_size, uint64 partition_count, _Atomic uint64* counted) {
+    uint64* alloc = malloc(9 * 8);
+    
+    alloc[0] = thread_id;
+    alloc[1] = thread_count;
+    alloc[2] = (uint64)count_reports;
+    alloc[3] = (uint64)input;
+    alloc[4] = (uint64)output;
+    alloc[5] = start_index;
+    alloc[6] = work_size;
+    alloc[7] = partition_count;
+    alloc[8] = (uint64)counted;
+
     return alloc;
 }
 
-void* call_partition_sequential(void* data) {
+void* call_count_then_move(void* data) {
     uint64* args = (uint64*)data;
 
-    uint64* input = *(uint64**)args;
-    uint64** output = *(uint64***)(args+1);
-    uint64 start_index = *(uint64*)(args+2);
-    uint64 work_size = *(uint64*)(args+3);
-    uint64 partition_count = *(uint64*)(args+4);
+    uint64 thread_id = *(uint64*)(args);
+    uint64 thread_count = *(uint64*)(args+1);
+    uint64* count_reports = *(uint64**)(args+2);
+    uint64* input = *(uint64**)(args+3);
+    uint64* output = *(uint64**)(args+4);
+    uint64 start_index = *(uint64*)(args+5);
+    uint64 work_size = *(uint64*)(args+6);
+    uint64 partition_count = *(uint64*)(args+7);
+    _Atomic uint64* counted = *(_Atomic uint64**)(args+8);
 
-    partition_sequential(input, output, start_index, work_size, partition_count);
+    count_then_move(thread_id, thread_count, count_reports, input, output, start_index, work_size, partition_count, counted);
     free(data);
 
     return NULL;
 }
 
-void partition_independant_output(uint64* input, uint64** output, uint64 input_size, uint64 threads, uint64 partition_count) {
+void partition_count_then_move(uint64* input, uint64** output, uint64 input_size, uint64 threads, uint64 partition_count) {
 
     uint64* partitions = malloc((sizeof(uint64) * partition_count) + ((2 * sizeof(uint64)) * input_size));
+    uint64* count_reports = malloc(sizeof(uint64) * threads * partition_count);
+    _Atomic uint64 counted = 0;
 
+// Start worker threads
     uint64 work_pr_thread = input_size / threads;
-    uint64* thread_buffers[threads];
     pthread_t thread_pids[threads];
     for(int t = 0; t < threads; t++) {
         uint64 start_index = t * work_pr_thread;
@@ -73,37 +97,25 @@ void partition_independant_output(uint64* input, uint64** output, uint64 input_s
         if(t == threads - 1) work_size = input_size - start_index;
         else work_size = work_pr_thread;
 
-        pthread_create(&thread_pids[t], NULL, call_partition_sequential, create_args(input, thread_buffers+t, start_index, work_size, partition_count));
+        pthread_create(&thread_pids[t], NULL, call_count_then_move, create_args(t, threads, count_reports, input, partitions+partition_count, start_index, work_size, partition_count, &counted));
     }
+
+// Await full counting phase completion
+    while(counted != threads);
+    counted = -1;
 
     for(int t = 0; t < threads; t++) {
         pthread_join(thread_pids[t], NULL);
     }
+
+// Write full partition sizes
+    for(uint64 p = 0; p < partition_count; p++) {
+        partitions[p] = 0;
+        for(uint64 t = 0; t < threads; t++)
+            partitions[p] += count_reports[(t*partition_count)+p];
+    }
     
-    for(int i = 0; i < partition_count; i++) {
-        partitions[i] = 0;
-        for(int t = 0; t < threads; t++) partitions[i] += thread_buffers[t][i];
-    }
-
-    int partition_indices[partition_count];
-    for(int i = 0; i < partition_count; i++) partition_indices[i] = 0;
-
-    for(int t = 0; t < threads; t++) {
-        for(int i = 0; i < partition_count; i++) {
-            uint64 thread_partition_size = thread_buffers[t][i];
-            uint64 thread_partition_offset = partition_count;
-                for(int p = 0; p < i; p++) thread_partition_offset += 2 * thread_buffers[t][p];
-            uint64 section_offset = partition_count;
-                for(int p = 0; p < i; p++) section_offset += 2 * partitions[p];
-
-            for(int s = 0; s < thread_partition_size; s++) {
-                int partition_index = 2 * partition_indices[i]++;
-                partitions[section_offset + partition_index] = thread_buffers[t][thread_partition_offset + (2*s)];
-                partitions[section_offset + partition_index + 1] = thread_buffers[t][thread_partition_offset + (2*s) + 1];
-            }
-        }
-        free(thread_buffers[t]);
-    }
+    free(count_reports);
 
     *output = partitions;
 }
@@ -117,7 +129,7 @@ int main() {
     init_utils(partition_count);
 
 // Generate data
-    uint64*  data = malloc((2 * sizeof(uint64)) * problem_size);
+    uint64* data = malloc((2 * sizeof(uint64)) * problem_size);
     char buffer[8];
     for(int i = 0; i < problem_size; i++) {
         getrandom(&buffer, 8, 0);
@@ -128,10 +140,12 @@ int main() {
 // Calculate partitions
     uint64* partitions;
     //partition_sequential(data, &partitions, 0, problem_size, partition_count);
-    partition_independant_output(data, &partitions, problem_size, 4, partition_count);
+    partition_count_then_move(data, &partitions, problem_size, 1, partition_count);
 
 // Print
     // print_partitions(partitions, partition_count, 1);
-    print_partition_distribution(partitions, partition_count, 150);
+    print_partition_distribution(partitions, partition_count, 100);
     print_partition_statistic(partitions, partition_count);
+
+    free(data);
 }
