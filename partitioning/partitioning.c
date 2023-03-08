0f932d65f148a8ffd4d11398f8c31318999e4fd1
Thomas Hoffmann Kilbak <thomaskilbak@gmail.com>
2023-03-08 16:32:10 +0100

    Add repeated tests

--- partitioning/partitioning.c
+++ partitioning/partitioning.c
@@ -38,19 +38,31 @@ long time_run(int algorithm, uint64 *data, int problem_size, uint64 partition_co
     return elapsed_time_ms;
 }
 
-void benchmark_all_combinations(uint64 *data, int problem_size, char *out_file_name) {
+void benchmark_all_combinations(int algorithm, uint64 *data, int problem_size, char *out_file_name, int repetitions, int up_to_hashbits, int up_to_threads) {
     FILE *fp;
     fp = fopen(out_file_name, "w");
-    fprintf(fp, " ,1,2,4,8,16,32\n");
-    for (int b = 1; b <= 18; b++) {
+
+    char top_row[30] = " ";
+    for (int i = 1; i <= up_to_threads; i *= 2) {
+        char as_string[4];
+        sprintf(as_string, ",%i", i);
+        strcat(top_row, as_string);
+    }
+    fprintf(fp, "%s\n", top_row);
+
+    for (int b = 1; b <= up_to_hashbits; b++) {
         char gathered_data[1024] = "";
         sprintf(gathered_data, "%i", b);
-        for (int t = 1; t <= 32; t *= 2) {
+        for (int t = 1; t <= up_to_threads; t *= 2) {
+            long sum_time = 0;
             strcat(gathered_data, ",");
-            uint64 partition_count = 1llu << b;
-            long time = time_run(CONCURRENT_OUTPUT, data, problem_size, partition_count, t);
+            for (int i = 0; i < repetitions; i++) {
+                uint64 partition_count = 1llu << b;
+                long time = time_run(algorithm, data, problem_size, partition_count, t);
+                sum_time += time;
+            }
             char as_string[10];
-            sprintf(as_string, "%ld", time);
+            sprintf(as_string, "%ld", (sum_time / repetitions));
             strcat(gathered_data, as_string);
         }
         strcat(gathered_data, "\n");
@@ -75,18 +87,21 @@ int main() {
     long elapsed_time_ms = (finish.tv_sec - start.tv_sec) * 1000 + (finish.tv_nsec - start.tv_nsec) / 1000000;
     printf("Data generation elapsed time: %lu ms\n", elapsed_time_ms);
 
-// Time count-then-move
-    long elapsed_time;
-    for (int run = 1; run <= 18; run++) {
-        elapsed_time = time_run(COUNT_THEN_MOVE, data, problem_size, partition_count, run);
-        printf("count-then-move #%i: %li ms\n", run, elapsed_time);
-    }
+    benchmark_all_combinations(CONCURRENT_OUTPUT, data, problem_size, "lots_of_data_concurrent.csv", 4, 18, 32);
+    benchmark_all_combinations(COUNT_THEN_MOVE, data, problem_size, "lots_of_data_countmove.csv", 4, 18, 32);
+    partition_count_then_move(data, problem_size, 16, partition_count);
+// // Time count-then-move
+//     long elapsed_time;
+//     for (int run = 1; run <= 18; run++) {
+//         elapsed_time = time_run(COUNT_THEN_MOVE, data, problem_size, partition_count, run);
+//         printf("count-then-move #%i: %li ms\n", run, elapsed_time);
+//     }
 
-// Time concurrent
-    for (int run = 1; run <= 18; run++) {
-        elapsed_time = time_run(CONCURRENT_OUTPUT, data, problem_size, partition_count, run);
-        printf("concurrent_output #%i: %li ms\n", run, elapsed_time);
-    }
+// // Time concurrent
+//     for (int run = 1; run <= 18; run++) {
+//         elapsed_time = time_run(CONCURRENT_OUTPUT, data, problem_size, partition_count, run);
+//         printf("concurrent_output #%i: %li ms\n", run, elapsed_time);
+//     }
 
 
 // // Calculate partitions
