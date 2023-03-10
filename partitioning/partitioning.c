34ddb5a805c916aef1db3e31769389b22dc8ec5f
Thomas Hoffmann Kilbak <thki@queue-it.com>
2023-03-10 13:11:07 +0100

    Clean up main function

--- partitioning/partitioning.c
+++ partitioning/partitioning.c
@@ -52,6 +52,7 @@ void benchmark_all_combinations(int algorithm, uint64 *data, int problem_size, c
     fprintf(fp, "%s\n", top_row);
 
     for (int b = 1; b <= up_to_hashbits; b++) {
+        printf("b: %i\n", b);
         char gathered_data[1024] = "";
         sprintf(gathered_data, "%i", b);
         for (int t = 1; t <= up_to_threads; t *= 2) {
@@ -71,48 +72,46 @@ void benchmark_all_combinations(int algorithm, uint64 *data, int problem_size, c
     }
 }
 
-int main() {
-// Setup
+void algorithm_correctness_check() {
+    int problem_size = 16777216;
+    uint64 b = 4;
+    uint64* data;
+    uint64 partition_count = 1llu << b;
+    init_utils(partition_count);
+    generate_data(&data, problem_size);
+
+    struct partition_info concurrent_info = partition_concurrent_output(data, problem_size, 16, partition_count);
+    struct partition_info countmove_info = partition_count_then_move(data, problem_size, 16, partition_count);
+    print_partition_statistic(concurrent_info, partition_count);
+    print_partition_statistic(countmove_info, partition_count);
+    printf("# correct results\n");
+    print_correct_stats(data, partition_count, problem_size);
+    free(data);
+}
+
+void run_benchmarks() {
     int problem_size = 16777216;
     uint64 b = 4;
     uint64 partition_count = 1llu << b;
-
     init_utils(partition_count);
 
-// Generate data
     uint64* data;
     struct timespec start, finish;
-    clock_gettime(CLOCK_MONOTONIC, &start);
+    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
     generate_data(&data, problem_size);
-    clock_gettime(CLOCK_MONOTONIC, &finish);
+    clock_gettime(CLOCK_MONOTONIC_RAW, &finish);
     long elapsed_time_ms = (finish.tv_sec - start.tv_sec) * 1000 + (finish.tv_nsec - start.tv_nsec) / 1000000;
     printf("Data generation elapsed time: %lu ms\n", elapsed_time_ms);
 
-    benchmark_all_combinations(CONCURRENT_OUTPUT, data, problem_size, "test_raw.csv", 2, 10, 16);
-    // benchmark_all_combinations(COUNT_THEN_MOVE, data, problem_size, "lots_of_data_countmove.csv", 4, 18, 32);
-
-// // Time count-then-move
-//     long elapsed_time;
-//     for (int run = 1; run <= 18; run++) {
-//         elapsed_time = time_run(COUNT_THEN_MOVE, data, problem_size, partition_count, run);
-//         printf("count-then-move #%i: %li ms\n", run, elapsed_time);
-//     }
-
-// // Time concurrent
-//     for (int run = 1; run <= 18; run++) {
-//         elapsed_time = time_run(CONCURRENT_OUTPUT, data, problem_size, partition_count, run);
-//         printf("concurrent_output #%i: %li ms\n", run, elapsed_time);
-//     }
-
-
-// // Calculate partitions
-//     uint64* partitions;
-//     partition_count_then_move(data, &partitions, problem_size, 1, partition_count);
-
-// // Print
-//     // print_partitions(partitions, partition_count, 1);
-//     print_partition_distribution(partitions, partition_count, 100);
-//     print_partition_statistic(partitions, partition_count);
-
+    printf("benching concurrent output\n");
+    benchmark_all_combinations(CONCURRENT_OUTPUT, data, problem_size, "./benchmark_data/10-3_concurrent.csv", 8, 18, 32);
+    printf("benching count-then-move\n");
+    benchmark_all_combinations(COUNT_THEN_MOVE, data, problem_size, "./benchmark_data/10-3_count-then-move.csv", 8, 18, 32);
     free(data);
+}
+
+int main() {
+    run_benchmarks();
+    // algorithm_correctness_check();
+    return 0;
 }
