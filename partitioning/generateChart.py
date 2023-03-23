302d18d447f24b615a2af06e812d416702943cdc
Thomas Hoffmann Kilbak <thomaskilbak@gmail.com>
2023-03-23 14:57:32 +0100

    Create lots of charts

--- partitioning/generateChart.py
+++ partitioning/generateChart.py
@@ -1,22 +1,38 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
+import sys
+import os
 
-df = pd.read_csv('./benchmark_data/server_8passes_concurrent.csv', names=['Category', 1, 2, 4, 8, 16, 32], skiprows=1)
+directory = sys.argv[1]
+file_list = os.listdir(directory)
 
-# Generate a bar chart of the data
-df.iloc[:, 1:] = 16777216 / (df.iloc[:, 1:]/1000)
-ax = df.plot(kind='line', x='Category', y=[1, 2, 4, 8, 16, 32], marker='o', legend=True)
-# Set the chart title and axes labels
-ax.set_title('')
-ax.grid(True)
-ax.set_xticks(range(1, 19))
-ax.set_xticklabels(range(1, 19))
-ax.set_xlabel('Hash bits')
+print(file_list)
+if not os.path.exists(directory + '/charts'):
+    os.makedirs(directory + '/charts')
 
-formatter = ticker.FuncFormatter(lambda x, pos: '{:.0f}'.format(round(x/1000000)))
-ax.yaxis.set_major_formatter(formatter)
-ax.set_ylabel('Million tuples per second')
+for file in file_list:
+    metric = file[:-4]
+    df = pd.read_csv(directory + "/" + file, names=['Category', 1, 2, 4, 8, 16, 32], skiprows=1)
+
+    ax = df.plot(kind='line', x='Category', y=[1, 2, 4, 8, 16, 32], marker='o', legend=True)
+    
+    if file.__contains__("timing"):
+        df.iloc[:, 1:] = 16777216 / (df.iloc[:, 1:]/1000)
+        ax.set_ylabel('Million tuples per second')
+        formatter = ticker.FuncFormatter(lambda x, pos: '{:.0f}'.format(round(x/1000000)))
+        ax.yaxis.set_major_formatter(formatter)
+    else:
+        ax.set_ylabel('Count')
+
+    ax.set_ylim(ymin=0)
+
+    ax.set_title(metric)
+    ax.grid(True)
+    ax.set_xticks(range(1, 19))
+    ax.set_xticklabels(range(1, 19))
+    ax.set_xlabel('Hash bits')
 
 
-plt.savefig('chart.png', bbox_inches='tight')
+
+    plt.savefig(directory + "/charts/" + metric + ".png", bbox_inches='tight')
