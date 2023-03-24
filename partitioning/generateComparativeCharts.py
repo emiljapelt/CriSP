b2ffa8d98a0e6284e23b9f55235c31716b3c4789
Thomas Hoffmann Kilbak <thki@queue-it.com>
2023-03-24 17:21:34 +0100

    Better comparative chart

--- partitioning/generateComparativeCharts.py
+++ partitioning/generateComparativeCharts.py
@@ -1,35 +1,38 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import sys
 import os
 
 directory = sys.argv[1]
-file_list = os.listdir(directory + "/CONCURRENT_OUTPUT")
-if not os.path.exists(directory + '/comparative_charts'):
-    os.makedirs(directory + '/comparative_charts')
+file_list = os.listdir(directory)
 
+if not os.path.exists(directory + '/charts'):
+    os.makedirs(directory + '/charts')
+
+data = {}
+
+# Loop through all files in the directory
 for file in file_list:
-    if not file.__contains__(".csv"):
+    if not os.path.isfile(directory + "/" + file):
         continue
-
-    metric = file[:-4]
-    df1 = pd.read_csv(directory + "/CONCURRENT_OUTPUT/" + file)
-    df2 = pd.read_csv(directory + "/COUNT_THEN_MOVE/" + file)
-
-
-    # extract the desired column from each dataframe
     
-    if file.__contains__("timing"):
-        col1 = df1['32'].apply(lambda x: 16777216 / (x/1000))
-        col2 = df2['32'].apply(lambda x: 16777216 / (x/1000))
-    else:
-        col1 = df1['32']
-        col2 = df2['32']
-    # plot the two columns using matplotlib
-    plt.xlim(1, 18)
-    plt.plot(col1, label='concurrent output', marker='o')
-    plt.plot(col2, label='count then move', marker='o')
-    plt.legend()
-    plt.savefig(directory + "/comparative_charts/" + metric + ".png", bbox_inches='tight')
-    plt.clf()
-    # plt.show()
+    # Read CSV file into a Pandas DataFrame
+    df = pd.read_csv(directory + "/" + file, index_col=0)
+
+    # Add the right-most column to the data dictionary with the metric name as key
+    metric = file[:-4]  # Remove '.csv' from file name
+    data[metric] = df.iloc[:, -1]
+
+# Create a new DataFrame from the data dictionary
+df = pd.DataFrame(data)
+
+# Plot the values in the '32' column of the DataFrame
+ax = df.plot(kind='line', marker='o', legend=True)
+plt.legend(title="Metric")
+ax.set_ylabel('Value')
+ax.set_title("Comparison of Metrics")
+ax.grid(True)
+ax.set_xlabel('Category')
+ax.set_ylim(ymin=0)
+plt.savefig(directory + "/charts/metrics_comparison.png", bbox_inches='tight')
