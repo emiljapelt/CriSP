da1dce947fd4b43c4a13f7e0f60d36a03eccb04b
Thomas Hoffmann Kilbak <thki@queue-it.com>
2023-05-04 12:07:37 +0200

    Set LF

--- merging/compile.sh
+++ merging/compile.sh
@@ -1,5 +1,5 @@
 clang -pthread ../utilities/pcg-c-0.94/extras/entropy.o ../utilities/pcg-c-0.94/src/pcg-advance-64.o ../utilities/pcg-c-0.94/src/pcg-advance-128.o ../utilities/pcg-c-0.94/src/pcg-rngs-128.o ../utilities/pcg-c-0.94/src/pcg-rngs-64.o ../utilities/utils.c ../utilities/data-gen.c ./implementations/merge-sort.c ./implementations/general-merge-sort.c ./merge.c -o ./merge-clang.exe
 gcc -pthread ../utilities/pcg-c-0.94/extras/entropy.o ../utilities/pcg-c-0.94/src/pcg-advance-64.o ../utilities/pcg-c-0.94/src/pcg-advance-128.o ../utilities/pcg-c-0.94/src/pcg-rngs-128.o ../utilities/pcg-c-0.94/src/pcg-rngs-64.o ../utilities/utils.c ../utilities/data-gen.c ./implementations/merge-sort.c ./implementations/general-merge-sort.c ./merge.c -o ./merge-gcc.exe
 # tcc -pthread ../utilities/pcg-c-0.94/extras/entropy.o ../utilities/pcg-c-0.94/src/pcg-advance-64.o ../utilities/pcg-c-0.94/src/pcg-advance-128.o ../utilities/pcg-c-0.94/src/pcg-rngs-128.o ../utilities/pcg-c-0.94/src/pcg-rngs-64.o ../utilities/utils.c ../utilities/data-gen.c ./implementations/merge-sort.c ./implementations/general-merge-sort.c ./merge.c -o ./merge-cc.exe
-tcc -pthread ./implementations/merge-sort.c  main.c  -o ./merge-tcc.exe
+gcc -pthread ./implementations/merge-sort.c  main.c  -o ./merge-tcc.exe
 #-Wall -g3 -fsanitize=address
