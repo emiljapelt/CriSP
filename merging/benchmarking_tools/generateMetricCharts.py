fa7b7838b0e8afcffdcd926c68b8e80356068882
Thomas Hoffmann Kilbak <thomaskilbak@gmail.com>
2023-05-05 13:12:28 +0200

    Add merging chart generator

---
+++ merging/benchmarking_tools/generateMetricCharts.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import sys
+import os
+
+# directory should be the path to the folder of either CONCURRENT_OUTPUT or COUNT_THEN_MOVE
+directory = sys.argv[1] + "/averages/"
+file_list = os.listdir(directory)
+# print(file_list)
+
+if not os.path.exists(directory + '/charts'):
+    os.makedirs(directory + '/charts')
+
+for file in file_list:
+    if not os.path.isfile(directory + "/" + file):
+        continue
+    
+    metric = file[:-4]
+    df = pd.read_csv(directory + file, names=['Category', "tcc", "clang-default", "clang-O2", "gcc-default", "gcc-O2", "gcc-O3", "gcc-Os"], skiprows=1)
+
+    ax = df.plot(kind='line', x='Category', y=["tcc", "clang-default", "clang-O2", "gcc-default", "gcc-O2", "gcc-O3", "gcc-Os"], marker='o', legend=True)
+    plt.legend(title="Compiler")
+    
+    ax.set_ylabel(metric)
+    ax.set_title(metric)
+
+    ax.set_ylim(ymin=0)
+
+    ax.grid(True)
+    ax.set_xticks(range(0, 6))
+    ax.set_xticklabels(range(0, 6))
+    ax.set_xlabel('Depth')
+
+
+
+    plt.savefig(directory + "/charts/" + metric + ".png", bbox_inches='tight')
