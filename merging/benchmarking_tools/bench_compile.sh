87e898009a54b5ed35bc0db1c4949924e6d28bed
Thomas Hoffmann Kilbak <thki@queue-it.com>
2023-05-11 10:51:22 +0200

    Update bench_compile

--- merging/benchmarking_tools/bench_compile.sh
+++ merging/benchmarking_tools/bench_compile.sh
@@ -1,8 +1,12 @@
-tcc -pthread ../implementations/merge-sort.c  main.c  -o ./out/merge-tcc-bench.exe
-clang -pthread ../implementations/merge-sort.c  main.c  -o ./out/merge-clang-default-bench.exe
-clang -O2 -pthread ../implementations/merge-sort.c  main.c  -o ./out/merge-clang-O2-bench.exe
-gcc -pthread ../implementations/merge-sort.c  main.c  -o ./out/merge-gcc-default-bench.exe
-gcc -O2 -pthread ../implementations/merge-sort.c  main.c  -o ./out/merge-gcc-O2-bench.exe
-gcc -O3 -pthread ../implementations/merge-sort.c  main.c  -o ./out/merge-gcc-O3-bench.exe
-gcc -Os -pthread ../implementations/merge-sort.c  main.c  -o ./out/merge-gcc-Os-bench.exe
+dir="../executables/out"
+if [ ! -d "$dir" ]; then
+    mkdir "$dir"
+fi
+tcc -pthread ../implementations/merge-sort.c  main.c  -o "$dir/merge-tcc-bench.exe"
+clang -pthread ../implementations/merge-sort.c  main.c  -o "$dir/merge-clang-default-bench.exe"
+clang -O2 -pthread ../implementations/merge-sort.c  main.c  -o "$dir/merge-clang-O2-bench.exe"
+gcc -pthread ../implementations/merge-sort.c  main.c  -o "$dir/merge-gcc-default-bench.exe"
+gcc -O2 -pthread ../implementations/merge-sort.c  main.c  -o "$dir/merge-gcc-O2-bench.exe"
+gcc -O3 -pthread ../implementations/merge-sort.c  main.c  -o "$dir/merge-gcc-O3-bench.exe"
+gcc -Os -pthread ../implementations/merge-sort.c  main.c  -o "$dir/merge-gcc-Os-bench.exe"
 #-Wall -g3 -fsanitize=address
